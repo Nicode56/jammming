c8233ff97d72ee5c8a19bb51595806ceba0f2396
Nicode56 <nakelledy@gmail.com>
2026-04-30 15:28:04 -0400

    Updated paths for execution

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,8 +1,8 @@
 import React from 'react'; // Removed unused { useState } since this is a Class component
 import styles from './App.module.css';
-import SearchBar from './component/searchbar/SearchBar.js';
-import Playlist from './component/playlist/Playlist.js';
-import SearchResults from '.component/searchresults/SearchResults.js';
+import SearchBar from '../searchbar/SearchBar';
+import Playlist from '../playlist/Playlist';
+import SearchResults from '../searchresults/SearchResults';
 import { Spotify } from '../../util/spotify/Spotify.js';
 
 class App extends React.Component {
